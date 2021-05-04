27d163bef0b6b2310d9cfd6c2c59ab7794989bca
Seamus Brown <shay_brown_13@hotmail.com>
2021-05-04 16:29:39 -0700

    add task adds to dynamodb, recyclerview displays dynamodb tasks

--- app/src/main/java/com/shaybrow/taskmaster1/addTask.java
+++ app/src/main/java/com/shaybrow/taskmaster1/addTask.java
@@ -4,39 +4,51 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 
-import com.shaybrow.taskmaster1.models.Task;
+import com.amplifyframework.api.graphql.model.ModelMutation;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.datastore.generated.model.Task;
 
 import java.util.Locale;
 
 public class addTask extends AppCompatActivity {
-    PopupWindow popUp;
+  static String TAG = "s.addTask";
     TaskDatabase taskDatabase;
-    int count =0;
+//    int count =0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
-        taskDatabase = Room.databaseBuilder(getApplicationContext(),TaskDatabase.class, "shaybrow_task_db" ).allowMainThreadQueries().build();
+//        taskDatabase = Room.databaseBuilder(getApplicationContext(),TaskDatabase.class, "shaybrow_task_db" ).allowMainThreadQueries().build();
 //        popUp = new PopupWindow(this);
 //        TextView textView = new TextView(this);
         Button button = findViewById(R.id.submitTask);
         button.setOnClickListener( view -> {
+            String title = ((EditText) findViewById(R.id.taskTitle)).getText().toString();
+            String body = ((EditText)findViewById(R.id.importTaskBody)).getText().toString();
+            Task task = Task.builder()
+            .title(title)
+            .body(body).build();
 
-            Task task = new Task(((EditText)findViewById(R.id.taskTitle)).getText().toString(), ((EditText)findViewById(R.id.importTaskBody)).getText().toString());
-            taskDatabase.taskDao().insert(task);
+//            taskDatabase.taskDao().insert(task);
 
-            count ++ ;
+//            count ++ ;
 //            String title = ((EditText)findViewById(R.id.taskTitle)).getText().toString();
 //            String body = ((EditText)findViewById(R.id.taskBody)).getText().toString();
             ((TextView)findViewById(R.id.submitted)).setVisibility(View.VISIBLE);
-            ((TextView) findViewById(R.id.taskTotal)).setText(String.format(Locale.ENGLISH, "Total Tasks" + count));
-
+//            ((TextView) findViewById(R.id.taskTotal)).setText(String.format(Locale.ENGLISH, "Total Tasks" + count));
+            Amplify.API.mutate(ModelMutation.create(task),
+                    response -> {
+                        Log.i(TAG, "onCreate: added");
+                    }, response ->{
+                        Log.i(TAG, "onCreate: miss");
+                    });
 
 //            Intent intent = new Intent(addTask.this, allTasks.class);
 //            intent.putExtra("taskTitle", title);
@@ -49,6 +61,6 @@ public class addTask extends AppCompatActivity {
     @Override
     protected void onResume (){
         super.onResume();
-        ((TextView) findViewById(R.id.taskTotal)).setText(String.format(Locale.ENGLISH, "Total Tasks" + count));
+//        ((TextView) findViewById(R.id.taskTotal)).setText(String.format(Locale.ENGLISH, "Total Tasks" + count));
     }
 }
